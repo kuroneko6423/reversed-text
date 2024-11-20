6e54e0da171bcb6f1ea2fb4de23e23f77e8f1ef4
zrpy <91083604+zrpy@users.noreply.github.com>
2024-11-20 23:49:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,8 @@ with open("a.txt", "r", encoding='UTF-8') as f:
 # b.txtを書き込みモードで開く
 with open('b.txt', 'w', encoding='UTF-8') as f:
     # ファイルの内容を1行ずつ書き込む
-    for line in reversed(lines):
+    for line in lines:
         # 文字を反転する
-        # reversed(line) は、文字列 line を逆順に並び替えたイテレータを返します。
-        # ''.join(何か) は、与えられたイテレータの要素を結合し、それらを 1 つの文字列に変換します。ここで、空文字列 '' が区切り文字として使われています。
-        reversed_line = ''.join(reversed(line))
+        converted_line=line[::-1]
         # ファイルに書き込む
-        f.write(reversed_line)
+        f.write(converted_line)
